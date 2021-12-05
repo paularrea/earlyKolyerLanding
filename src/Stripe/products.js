0e6836ab301bbc925c4da0489d38d1d3848c7283
Pau Larrea <paularrea12@hotmail.com>
2021-12-05 11:21:30 +0100

    urls

--- src/Stripe/products.js
+++ src/Stripe/products.js
@@ -4,39 +4,39 @@ export const stripeProducts = (productsArr) => {
       case "black":
         switch (productsArr[0].size) {
           case "S":
-            return "blackUrlSmall";
+            return "https://buy.stripe.com/eVa3cV8oT8Cg2TSeV6";
           case "M":
-            return "blackUrlMedium";
+            return "https://buy.stripe.com/28odRzgVp9GkfGEbIV";
           case "L":
-            return "blackUrlL";
+            return "https://buy.stripe.com/28odRz48D05K9ig7sG";
           case "XL":
-            return "blackUrlX";
+            return "https://buy.stripe.com/14kcNv8oT05KeCA14j";
           default:
             return;
         }
       case "blue":
         switch (productsArr[0].size) {
           case "S":
-            return "blueUrlSmall";
-          case "M":
-            return "blueUrlMedium";
-          case "L":
-            return "blueUrlL";
-          case "XL":
-            return "blueUrlX";
+            return "https://buy.stripe.com/8wMfZHfRlg4IbqocMQ";
+            case "M":
+              return "https://buy.stripe.com/dR6dRzfRldWA520eUZ";
+            case "L":
+              return "https://buy.stripe.com/3cs3cV0Wr19OgKIaEK";
+            case "XL":
+              return "https://buy.stripe.com/6oE00J34z7yc2TS28f";
           default:
             return;
         }
       case "red":
         switch (productsArr[0].size) {
           case "S":
-            return "redUrlSmall";
+            return "https://buy.stripe.com/3cs3cV20vdWA6647sA";
           case "M":
-            return "redUrlMedium";
+            return "https://buy.stripe.com/eVaeVD20vg4I5206ox";
           case "L":
-            return "redUrlL";
+            return "https://buy.stripe.com/4gw14N6gLcSwcus3cm";
           case "XL":
-            return "redUrlX";
+            return "https://buy.stripe.com/eVa00JcF9bOs2TS6oz";
           default:
             return;
         }
@@ -47,111 +47,111 @@ export const stripeProducts = (productsArr) => {
       case "blackblack":
         switch (productsArr[0].size + productsArr[1].size) {
           case "SS":
-            return "blackblackSS";
+            return "https://buy.stripe.com/5kA28R7kP3hWfGE4h2";
           case "SM":
-            return "blackblackSM";
+            return "https://buy.stripe.com/eVa14NfRl6u8bqobJv";
           case "SL":
-            return "blackblackSL";
+            return "https://buy.stripe.com/dR614NbB53hW3XW14S";
           case "SXL":
-            return "blackblackSXL";
+            return "https://buy.stripe.com/8wM6p7dJd19O7a83d1";
           case "MS":
-            return "blackblackMS";
+            return "https://buy.stripe.com/5kA3cV7kPaKo0LKfZO";
           case "MM":
-            return "blackUrlL";
+            return "https://buy.stripe.com/eVabJr0WrbOsamk7tj";
           case "ML":
-            return "blackUrlX";
+            return "https://buy.stripe.com/fZe6p79sX05KeCAdRI";
           case "MXL":
-            return "blackUrlMedium";
+            return "https://buy.stripe.com/9AQ7tb34z6u80LK291";
           case "LS":
-            return "blackUrlL";
+            return "https://buy.stripe.com/7sI00J6gLf0EeCA7tm";
           case "LM":
-            return "blackUrlX";
+            return "https://buy.stripe.com/14kdRz48D6u866400V";
           case "LL":
-            return "blackUrlL";
+            return "https://buy.stripe.com/cN2cNv48D6u83XW150";
           case "LXL":
-            return "blackUrlX";
+            return "https://buy.stripe.com/6oE4gZdJd5q4dyw7tp";
           case "XLS":
-            return "blackUrlMedium";
+            return "https://buy.stripe.com/6oE8xfeNhf0EeCA8xu";
           case "XLM":
-            return "blackUrlL";
+            return "https://buy.stripe.com/eVa9Bjax15q4eCA8xv";
           case "XLL":
-            return "blackUrlX";
+            return "https://buy.stripe.com/8wM6p7cF99Gkcus5lk";
           case "XLXL":
-            return "blackUrlX";
+            return "https://buy.stripe.com/eVaeVD34zcSwamk6pp";
           default:
             return;
         }
       case "blackblue":
         switch (productsArr[0].size + productsArr[1].size) {
           case "SS":
-            return "blackblackSS";
+            return "https://buy.stripe.com/28o6p76gL5q40LK7tK";
           case "SM":
-            return "blackblackSM";
+            return "https://buy.stripe.com/9AQbJrbB57ycfGEbK4";
           case "SL":
-            return "blackblackSL";
+            return "https://buy.stripe.com/3cs3cV20vdWA5208xW";
           case "SXL":
-            return "";
+            return "https://buy.stripe.com/14k5l3dJdbOsdyw8y0";
           case "MS":
-            return "blackblackMS";
+            return "https://buy.stripe.com/5kAeVD7kP2dSdywaFX";
           case "MM":
-            return "blackUrlL";
+            return "https://buy.stripe.com/cN2dRz8oTdWAcus4hD";
           case "ML":
-            return "blackUrlX";
+            return "https://buy.stripe.com/eVa3cVfRl8Cg52001r";
           case "MXL":
-            return "blackUrlMedium";
+            return "https://buy.stripe.com/28ocNveNh8CgdywaG9";
           case "LS":
-            return "blackUrlL";
+            return "https://buy.stripe.com/4gwdRz7kP3hW8ec15o";
           case "LM":
-            return "blackUrlX";
+            return "https://buy.stripe.com/4gwfZH5cHcSw9ig9BY";
           case "LL":
-            return "blackUrlL";
+            return "https://buy.stripe.com/5kA9Bj48Dg4IfGEbKa";
           case "LXL":
-            return "blackUrlX";
+            return "https://buy.stripe.com/7sIdRzdJdaKogKI8y2";
           case "XLS":
-            return "https://buy.stripe.com/dR628Rax18CgfGE4iw";
+            return "https://buy.stripe.com/4gwfZHax13hWgKI9BV";
           case "XLM":
-            return "https://buy.stripe.com/8wM3cV8oT8Cgamk9CR";
+            return "https://buy.stripe.com/dR614N5cHcSw2TS6pN";
           case "XLL":
-            return "https://buy.stripe.com/cN26p7cF9g4IamkaGW";
+            return "https://buy.stripe.com/00g00JfRl4m0eCA7tV";
           case "XLXL":
-            return "https://buy.stripe.com/28o00JfRl3hW7a86qH";
+            return "https://buy.stripe.com/9AQdRz0Wrg4I2TS01x";
           default:
             return;
         }
       case "blueblack":
         switch (productsArr[0].size + productsArr[1].size) {
           case "SS":
-            return "blackblackSS";
+            return "https://buy.stripe.com/28o6p76gL5q40LK7tK";
           case "SM":
-            return "blackblackSM";
+            return "https://buy.stripe.com/5kAeVD7kP2dSdywaFX";
           case "SL":
-            return "blackblackSL";
+            return "https://buy.stripe.com/4gwdRz7kP3hW8ec15o";
           case "SXL":
-            return "https://buy.stripe.com/dR628Rax18CgfGE4iw";
+            return "https://buy.stripe.com/4gwfZHax13hWgKI9BV";
           case "MS":
-            return "blackblackMS";
+            return "https://buy.stripe.com/9AQbJrbB57ycfGEbK4";
           case "MM":
-            return "blackUrlL";
+            return "https://buy.stripe.com/cN2dRz8oTdWAcus4hD";
           case "ML":
-            return "blackUrlX";
+            return "https://buy.stripe.com/4gwfZH5cHcSw9ig9BY";
           case "MXL":
-            return "https://buy.stripe.com/8wM3cV8oT8Cgamk9CR";
+            return "https://buy.stripe.com/dR614N5cHcSw2TS6pN";
           case "LS":
-            return "blackUrlL";
+            return "https://buy.stripe.com/3cs3cV20vdWA5208xW";
           case "LM":
-            return "blackUrlX";
+            return "https://buy.stripe.com/eVa3cVfRl8Cg52001r";
           case "LL":
-            return "blackUrlL";
+            return "https://buy.stripe.com/5kA9Bj48Dg4IfGEbKa";
           case "LXL":
-            return "https://buy.stripe.com/cN26p7cF9g4IamkaGW";
+            return "https://buy.stripe.com/00g00JfRl4m0eCA7tV";
           case "XLS":
-            return "blackUrlMedium";
+            return "https://buy.stripe.com/14k5l3dJdbOsdyw8y0";
           case "XLM":
-            return "blackUrlL";
+            return "https://buy.stripe.com/28ocNveNh8CgdywaG9";
           case "XLL":
-            return "blackUrlX";
+            return "https://buy.stripe.com/7sIdRzdJdaKogKI8y2";
           case "XLXL":
-            return "https://buy.stripe.com/28o00JfRl3hW7a86qH";
+            return "https://buy.stripe.com/9AQdRz0Wrg4I2TS01x";
           default:
             return;
         }
@@ -232,148 +232,148 @@ export const stripeProducts = (productsArr) => {
       case "blueblue":
         switch (productsArr[0].size + productsArr[1].size) {
           case "SS":
-            return "blackblackSS";
+            return "https://buy.stripe.com/5kAfZH5cH19OgKI4gw";
           case "SM":
-            return "blackblackSM";
+            return "https://buy.stripe.com/cN24gZdJdbOsamk8wN";
           case "SL":
-            return "blackblackSL";
+            return "https://buy.stripe.com/fZe9Bj7kP05Kamk14m";
           case "SXL":
-            return "blackblackSXL";
+            return "https://buy.stripe.com/3cs00J8oT7ycamkbJ1";
           case "MS":
-            return "blackblackMS";
+            return "https://buy.stripe.com/00g7tb7kPcSw5205kE";
           case "MM":
-            return "blackUrlL";
+            return "https://buy.stripe.com/fZefZHcF99Gk9ig5kF";
           case "ML":
-            return "blackUrlX";
+            return "https://buy.stripe.com/aEU8xf7kP9Gkdyw7sO";
           case "MXL":
-            return "blackUrlMedium";
+            return "https://buy.stripe.com/9AQaFn5cH4m00LKaF1";
           case "LS":
-            return "blackUrlL";
+            return "https://buy.stripe.com/3csfZHax15q48ec00o";
           case "LM":
-            return "blackUrlX";
+            return "https://buy.stripe.com/fZe6p7bB5cSw3XWcNb";
           case "LL":
-            return "blackUrlL";
+            return "https://buy.stripe.com/6oEbJr48DdWA1PO4gG";
           case "LXL":
-            return "blackUrlX";
+            return "https://buy.stripe.com/8wMeVD34z7yc1PO9B1";
           case "XLS":
-            return "blackUrlMedium";
+            return "https://buy.stripe.com/5kA4gZdJddWAfGEfZq";
           case "XLM":
-            return "blackUrlL";
+            return "https://buy.stripe.com/fZe14N20vf0E1POdRj";
           case "XLL":
-            return "blackUrlX";
+            return "https://buy.stripe.com/14k3cV20v19O2TScNg";
           case "XLXL":
-            return "blackUrlX";
+            return "https://buy.stripe.com/9AQ3cV5cHf0E0LK14z";
           default:
             return;
         }
       case "redblue":
         switch (productsArr[0].size + productsArr[1].size) {
           case "SS":
-            return "blackblackSS";
+            return "https://buy.stripe.com/28o6p7gVpbOsgKIaFG";
           case "SM":
-            return "blackblackSM";
+            return "https://buy.stripe.com/7sI00JeNhaKo3XWcNS";
           case "SL":
-            return "blackblackSL";
+            return "https://buy.stripe.com/dR65l3eNh2dS2TS7tC";
           case "SXL":
-            return "blackblackSXL";
+            return "https://buy.stripe.com/6oE6p7bB52dS0LK15i";
           case "MS":
-            return "blackblackMS";
+            return "https://buy.stripe.com/fZe3cVdJdbOs1PO4hj";
           case "MM":
-            return "blackUrlL";
+            return "https://buy.stripe.com/9AQ7tbeNh4m0amk15b";
           case "ML":
-            return "blackUrlX";
+            return "https://buy.stripe.com/14k00JcF94m00LKcNX";
           case "MXL":
-            return "blackUrlMedium";
+            return "https://buy.stripe.com/4gw8xf9sXf0E3XW4hv";
           case "LS":
-            return "blackUrlL";
+            return "https://buy.stripe.com/28o7tb48DbOs7a8aFI";
           case "LM":
-            return "blackUrlX";
+            return "https://buy.stripe.com/9AQfZH7kPf0EeCA29g";
           case "LL":
-            return "blackUrlL";
+            return "https://buy.stripe.com/4gw00JbB5cSwfGE3do";
           case "LXL":
-            return "blackUrlX";
+            return "https://buy.stripe.com/8wMbJrdJd8Cg9ig5lA";
           case "XLS":
-            return "blackUrlMedium";
+            return "https://buy.stripe.com/3cs5l30WrcSwbqo8xB";
           case "XLM":
-            return "blackUrlL";
+            return "https://buy.stripe.com/bIYaFneNh5q4664g07";
           case "XLL":
-            return "blackUrlX";
+            return "https://buy.stripe.com/6oEdRz9sX6u8fGEeW7";
           case "XLXL":
-            return "";
+            return "https://buy.stripe.com/fZebJr7kP3hWamk9BR";
           default:
             return;
         }
       case "bluered":
         switch (productsArr[0].size + productsArr[1].size) {
           case "SS":
-            return "blackblackSS";
+            return "https://buy.stripe.com/28o6p7gVpbOsgKIaFG";
           case "SM":
-            return "blackblackSM";
+            return "https://buy.stripe.com/fZe3cVdJdbOs1PO4hj";
           case "SL":
-            return "blackblackSL";
+            return "https://buy.stripe.com/28o7tb48DbOs7a8aFI";
           case "SXL":
-            return "blackblackSXL";
+            return "https://buy.stripe.com/3cs5l30WrcSwbqo8xB";
           case "MS":
-            return "blackblackMS";
+            return "https://buy.stripe.com/7sI00JeNhaKo3XWcNS";
           case "MM":
-            return "blackUrlL";
+            return "https://buy.stripe.com/9AQ7tbeNh4m0amk15b";
           case "ML":
-            return "blackUrlX";
+            return "https://buy.stripe.com/9AQfZH7kPf0EeCA29g";
           case "MXL":
-            return "blackUrlMedium";
+            return "https://buy.stripe.com/bIYaFneNh5q4664g07";
           case "LS":
-            return "blackUrlL";
+            return "https://buy.stripe.com/dR65l3eNh2dS2TS7tC";
           case "LM":
-            return "blackUrlX";
+            return "https://buy.stripe.com/14k00JcF94m00LKcNX";
           case "LL":
-            return "blackUrlL";
+            return "https://buy.stripe.com/4gw00JbB5cSwfGE3do";
           case "LXL":
-            return "blackUrlX";
+            return "https://buy.stripe.com/6oEdRz9sX6u8fGEeW7";
           case "XLS":
-            return "blackUrlMedium";
+            return "https://buy.stripe.com/6oE6p7bB52dS0LK15i";
           case "XLM":
-            return "blackUrlL";
+            return "https://buy.stripe.com/4gw8xf9sXf0E3XW4hv";
           case "XLL":
-            return "blackUrlX";
+            return "https://buy.stripe.com/8wMbJrdJd8Cg9ig5lA";
           case "XLXL":
-            return "blackUrlX";
+            return "https://buy.stripe.com/fZebJr7kP3hWamk9BR";
           default:
             return;
         }
       case "redred":
         switch (productsArr[0].size + productsArr[1].size) {
           case "SS":
-            return "redredSS";
+            return "https://buy.stripe.com/8wMcNvdJd4m0eCA9B6";
           case "SM":
-            return "redredSM";
+            return "https://buy.stripe.com/28oeVDcF9cSwfGE00x";
           case "SL":
-            return "redredSL";
+            return "https://buy.stripe.com/aEUdRzcF94m02TS7t0";
           case "SXL":
-            return "redredSXL";
+            return "https://buy.stripe.com/14k28R20v9Gkdyw5kT";
           case "MS":
-            return "redredMS";
+            return "https://buy.stripe.com/5kAcNv8oTbOseCAeVu";
           case "MM":
-            return "redredMM";
+            return "https://buy.stripe.com/eVaaFnfRlg4Ibqo9Bb";
           case "ML":
-            return "redredML";
+            return "https://buy.stripe.com/6oEeVDgVpg4I5204gS";
           case "MXL":
-            return "redredMXL";
+            return "https://buy.stripe.com/3csaFn8oT7ycfGE4gT";
           case "LS":
-            return "redredLS";
+            return "https://buy.stripe.com/eVa28RbB5bOs9igbJm";
           case "LM":
-            return "redredLM";
+            return "https://buy.stripe.com/aEUbJrbB5cSw664dRv";
           case "LL":
-            return "redredLL";
+            return "https://buy.stripe.com/6oE28Rax13hWdyw4gW";
           case "LXL":
-            return "redredLXL";
+            return "https://buy.stripe.com/5kA3cVeNhcSw3XW28P";
           case "XLS":
-            return "redredXLS";
+            return "https://buy.stripe.com/bIY7tb9sX3hW520fZG";
           case "XLM":
-            return "redredXLM";
+            return "https://buy.stripe.com/00gdRzgVp19O664bJr";
           case "XLL":
-            return "redredXLL";
+            return "https://buy.stripe.com/4gw00JeNhaKo0LKeVE";
           case "XLXL":
-            return "redredXLXL";
+            return "https://buy.stripe.com/5kAfZH48DbOsamk6p9";
           default:
             return;
         }
